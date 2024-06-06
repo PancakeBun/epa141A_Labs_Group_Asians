8ffe7e3dd19f5c0a544ed511a7a6a47d83b6a97f
JZ <joeyzyz133@gmail.com>
2024-06-06 16:48:16 +0200

    Slurm script and test file
    Please change email in the slurm script before submitting jobs. Submit job use "sbatch slurm.sh"

---
+++ hpc_script/slurm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+#SBATCH --job-name="python_test"
+#SBATCH --account=education-tpm-msc-epa
+#SBATCH --mail-user=z.zhang-96@student.tudelft.nl
+#SBATCH --mail-type=all
+
+#SBATCH --partition=compute
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+
+module load 2023r1
+module load openmpi
+module load python
+module load py-numpy
+module load py-scipy
+module load py-mpi4py
+module load py-pip
+
+python -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade ema_workbench
+python -m pip install ipyparallel
+
+python ./test.py
+# mpiexec -n 1 python3 ./test.py
---
+++ hpc_script/test.py
@@ -0,0 +1,4 @@
+import ema_workbench
+print("ema_workbench")
+
+print("Hello, Delftblue")
